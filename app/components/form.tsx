33b01e828c4771dbcf3014c161be5d0c057931f2
Tiago Rocha <tiagorocha@portalctech.com.br>
2024-05-19 01:08:12 -0300

    fix(user): verificando no host qual é o usuário

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -4,40 +4,55 @@ import { ChangeEvent, useState } from "react"
 
 export const Form = () => {
     const [name, setName] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+    const [successMessage, setSuccessMessage] = useState('')
+    const [loading, setLoading] = useState(false)
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setErrorMessage('')
         // Replace invalid characters with an empty string
         const cleanedName = e.target.value.replace(/[^a-zA-Z0-9-]/g, '');
         console.log({ cleanedName });
 
-        setName(cleanedName);
+        setName(cleanedName.toLocaleLowerCase());
     };
 
     const handleSave = async () => {
         if (!name) return
+        try {
+            setLoading(true)
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dns`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ slug: name })
+            });
 
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dns`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ slug: name })
-        });
+            if (!response.ok) {
+                const error = await response.json();
+                console.log(`Error: ${response.status} - ${error}`);
+                setErrorMessage(error.message || JSON.stringify(error))
+            }
 
-        if (!response.ok) {
-            const error = await response.text();
-            console.log(`Error: ${response.status} - ${error}`);
+            const result = await response.json();
+            console.log(result);
+            setSuccessMessage(result.domain || '')
+            setName('')
+            setLoading(false)
+
+        } catch (error) {
+            setLoading(false)
         }
-
-        const result = await response.json();
-        console.log(result);
     }
 
     return <>
+        {successMessage && <a href={successMessage} target="_blank">Acessar novo subdomínio {successMessage}</a>}
         <fieldset className="flex flex-col">
             <label>Criar novo usuário</label>
             <input onChange={handleChange} value={name} className="py-1 px-4 rounded" />
-        </fieldset>
-        <button onClick={handleSave} type="button" className="bg-red-600 text-white py-1 px-4 rounded">Criar</button>
+            {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+        </fieldset >
+        <button onClick={handleSave} type="button" className="bg-red-600 text-white py-1 px-4 rounded" disabled={loading}>Criar</button>
     </>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import { Form } from "./components/form";
+import { headers } from "next/headers";
+
+function extractSubdomain(host: string): string | null {
+  const parts = host.split('.');
+  if (parts.length >= 3) {
+    return parts[0];
+  }
+  return null;
+}
 
 export default function Home() {
+  const headersList = headers();
+  const host = headersList.get('host') || '';
+  const subdomain = extractSubdomain(host) || 'unknown';
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-5">
-      Acessando usuário
+      Acessando usuário: {subdomain}
       <Form />
     </main>
   );
